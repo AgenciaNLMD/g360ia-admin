bd9411f2e9c3bcd476db8c1aa3086d74fb0ad66f
PAblo <agencianlmd@gmail.com>
2026-03-19 19:39:47 -0300

    Update route.js

--- app/api/integraciones/route.js
+++ app/api/integraciones/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
-import db from "../../../../lib/db";
+import { authOptions } from "../auth/[...nextauth]/route";
+import db from "../../../lib/db";
 
 export async function GET(request) {
   const session = await getServerSession(authOptions);
